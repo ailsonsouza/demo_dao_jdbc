5080e959adb7817e8ad86c29755a2782ff15d323
ailsonsouza <ailsonsouzapereira2@gmail.com>
2025-12-01 20:22:54 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -13,31 +13,29 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
 
 	private Connection conn;
-	
+
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public void insert(Seller obj) {
 		PreparedStatement st = null;
-		
+
 		try {
-			st = conn.prepareStatement("insert into seller "
-										+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-										+ "values (?, ?, ?, ?, ?)",
-										+ PreparedStatement.RETURN_GENERATED_KEYS);
+			st = conn.prepareStatement("insert into seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "values (?, ?, ?, ?, ?)", +PreparedStatement.RETURN_GENERATED_KEYS);
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
-			
+
 			int rowsAffected = st.executeUpdate();
-			
+
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if (rs.next()) {
@@ -45,38 +43,52 @@ public class SellerDaoJDBC implements SellerDao{
 					obj.setId(id);
 				}
 				DB.closeResultSet(rs);
-			}else {
+			} else {
 				throw new DbException("Update failed!");
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}finally {
+		} finally {
 			DB.closeStatement(st);
 		}
 
-		
-		
-		
-		
 	}
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement("update seller set Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "where Id = ?");
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = conn.prepareStatement("SELECT seller.*, department.Name as DepName FROM seller INNER JOIN department "
 					+ "ON seller.DepartmentId = department.iD	WHERE seller.Id = ?");
@@ -88,17 +100,14 @@ public class SellerDaoJDBC implements SellerDao{
 				return seller;
 			}
 			return null;
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
-			
+
 		}
-		
-		
+
 	}
 
 	@Override
@@ -106,87 +115,74 @@ public class SellerDaoJDBC implements SellerDao{
 		List<Seller> sellers = new ArrayList<Seller>();
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
-			st = conn.prepareStatement("select seller.*, department.Name as DepName from seller inner join department "
-										+ "order by Name");
-			
+			st = conn.prepareStatement(
+					"select seller.*, department.Name as DepName from seller inner join department " + "order by Name");
+
 			rs = st.executeQuery();
 			Department department2 = null;
-			
+
 			while (rs.next()) {
 				if (department2 == null) {
-					department2 = instantiateDepartment(rs);					
+					department2 = instantiateDepartment(rs);
 				}
-				sellers.add(instantiateSeller(rs, department2));	
+				sellers.add(instantiateSeller(rs, department2));
 			}
 
-			
 			return sellers;
-			
-			
-		}catch(SQLException e) {
-			throw new DbException (e.getMessage());
-		}finally {
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
-			
+
 		}
 	}
-	
+
 	@Override
 	public List<Seller> findByDepartment(int departmentId) {
 		List<Seller> sellers = new ArrayList<Seller>();
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = conn.prepareStatement("select seller.*, department.Name as DepName from seller inner join department "
-										+ "on seller.DepartmentId = department.Id where DepartmentId = ? order by Name");
+					+ "on seller.DepartmentId = department.Id where DepartmentId = ? order by Name");
 			st.setInt(1, departmentId);
-			
+
 			rs = st.executeQuery();
 			Department department2 = null;
-			
+
 			while (rs.next()) {
 				if (department2 == null) {
-					department2 = instantiateDepartment(rs);					
+					department2 = instantiateDepartment(rs);
 				}
-				sellers.add(instantiateSeller(rs, department2));	
+				sellers.add(instantiateSeller(rs, department2));
 			}
 
-			
 			return sellers;
-			
-			
-		}catch(SQLException e) {
-			throw new DbException (e.getMessage());
-		}finally {
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
-			
+
 		}
-		
+
 	}
-	
 
 	private Seller instantiateSeller(ResultSet rs, Department department) throws SQLException {
-		return new Seller(rs.getInt("Id"),
-				rs.getString("Name"),
-				rs.getString("Email"),
-				rs.getDate("BirthDate"),
-				rs.getDouble("BaseSalary"),
-				department);
+		return new Seller(rs.getInt("Id"), rs.getString("Name"), rs.getString("Email"), rs.getDate("BirthDate"),
+				rs.getDouble("BaseSalary"), department);
 	}
 
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
-		
+
 		return new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
-		
+
 	}
-
-
-
-	
 
 }
