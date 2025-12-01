4e10b79f90906c52db6239f2c03dd67baab735e5
ailsonsouza <ailsonsouzapereira2@gmail.com>
2025-12-01 20:45:29 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,11 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("=== TEST 6: seller delete ===");
+		seller = sellerDao.findById(10);
+		sellerDao.deleteById(20);
+		System.out.println("delete completed!");
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -59,16 +59,15 @@ public class SellerDaoJDBC implements SellerDao {
 		PreparedStatement st = null;
 
 		try {
-			st = conn.prepareStatement("update seller set Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
-					+ "where Id = ?");
+			st = conn.prepareStatement(
+					"update seller set Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+							+ "where Id = ?");
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
 			st.setInt(6, obj.getId());
-			
-			st.executeUpdate();
 
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
@@ -80,7 +79,23 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement("delete from seller where Id = ?");
+			st.setInt(1, id);
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected < 1) {
+				throw new DbException("No rows affected!");
+			}
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
